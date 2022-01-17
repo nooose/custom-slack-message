5ee075f437207a8a413783759b77c9541a75c158
mz01-seongjh <test@test.com>
2022-01-17 17:19:09 +0900

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,3 +5,97 @@ TYPE=$2
 
 echo "SLACK_WEBHOOK" $SLACK_WEBHOOK
 echo "TYPE" $TYPE
+echo "PR_NUMBER" $PR_NUMBER
+
+
+
+
+# pr
+if [ $TYPE == "pr" ]; then
+COLOR=\#A0A0A0
+
+
+cat << EOF > payload.json
+{
+	"attachments": [
+		{
+			"color": "${COLOR}",
+			"blocks": [
+				{
+					"type": "header",
+					"text": {
+						"type": "plain_text",
+						"text": "test-service"
+					}
+				},
+				{
+					"type": "section",
+					"text": {
+						"type": "mrkdwn",
+						"text": "*`develop`*  :arrow_left:  *`api/*`*"
+					}
+				},
+				{
+					"type": "section",
+					"text": {
+						"type": "mrkdwn",
+						"text": "<https://google.com|*PR 메시지*>"
+					}
+				},
+				{
+					"type": "divider"
+				},
+				{
+					"type": "section",
+					"text": {
+						"type": "mrkdwn",
+						"text": "*PR 생성자*"
+					}
+				},
+				{
+					"type": "context",
+					"elements": [
+						{
+							"type": "plain_text",
+							"text": "성준혁"
+						}
+					]
+				},
+				{
+					"type": "section",
+					"text": {
+						"type": "mrkdwn",
+						"text": "*PR 승인자*"
+					}
+				},
+				{
+					"type": "context",
+					"elements": [
+						{
+							"type": "plain_text",
+							"text": "성준혁"
+						},
+						{
+							"type": "plain_text",
+							"text": "성준혁"
+						},
+						{
+							"type": "plain_text",
+							"text": "성준혁"
+						}
+					]
+				}
+			]
+		}
+	]
+}
+EOF
+
+else if [ $TYPE == "build" ]; then
+
+
+
+else if [ $TYPE == "helm" ]; then
+
+
+fi
