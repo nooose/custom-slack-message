5963f3d76478f9930e77f6e1a13fdb2f36036e7b
성준혁 <36617783+nooose@users.noreply.github.com>
2022-01-28 13:48:32 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -138,7 +138,7 @@ cat << EOF > commit_field.json
     "elements": [
         {
             "type": "mrkdwn",
-            "text": "${COMMIT_MESSAGE} <${COMMIT_URL}|${COMMITTER}>"
+            "text": "• *${COMMIT_MESSAGE}* <${COMMIT_URL}|${COMMITTER}>"
         }
     ]
 }
