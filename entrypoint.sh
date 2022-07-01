8483bda6c1c74afc63c4eec7ad48a879bf41dc53
nooose <hd15807@gmail.com>
2022-07-01 17:43:15 +0900

    fix: sed 딜리미터 변경

--- entrypoint.sh
+++ entrypoint.sh
@@ -38,7 +38,7 @@ function create_build_payload() {
     sed -i -e "s@SENDER_AVATAR_URL@$SENDER_AVATAR_URL@g" /build_payload.json
     sed -i -e "s@SENDER_HTML_URL@$SENDER_HTML_URL@g" /build_payload.json
     sed -i -e "s@SENDER_NAME@$SENDER_NAME@g" /build_payload.json
-    sed -i -e "s@TAG/$TAG@g" /build_payload.json
+    sed -i -e "s@TAG@$TAG@g" /build_payload.json
     sed -i -e "s@BRANCH_NAME@$BRANCH_NAME@g" /build_payload.json
     sed -i -e "s@ACTION_URL@$ACTION_URL@g" /build_payload.json
     sed -i -e "s@GITHUB_WORKFLOW@$GITHUB_WORKFLOW@g" /build_payload.json
